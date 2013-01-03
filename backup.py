8e31e015baf93230dffb21d7dc90a4153f071dd4
Ang Way Chuang <wcang79@gmail.com>
2013-01-03 10:36:42 +0800

    Add support for file name glob

--- backup.py
+++ backup.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import stat
+import glob
 
 if len(sys.argv) != 3:
     print "Usage %s configuration destination" % sys.argv[0]
@@ -28,14 +29,18 @@ for temp in file_list:
     temp_file = open(temp['list'], 'r')
 
     for src in [ line.strip() for line in temp_file.readlines() if not line.startswith('#') ]:
-        mode = os.stat(src).st_mode
-        final_dest = dest + os.path.dirname(src)
+        expanded_src = glob.glob(src)
 
-        try:
-            os.stat(final_dest) #check whether the final destination exists
-        except Exception:
-            os.makedirs(final_dest) #create one
-        finally:
-            os.system("rsync -azvv --delete %s %s" % (src, final_dest))
+        for individual_src in expanded_src:
+            mode = os.stat(individual_src).st_mode
+            final_dest = dest + os.path.dirname(individual_src)
+
+            try:
+                os.stat(final_dest) #check whether the final destination exists
+            except Exception:
+                os.makedirs(final_dest) #create one
+            finally:
+                os.system("rsync -azvv --delete \"%s\" \"%s\"" % 
+                        (individual_src, final_dest))
 
     temp_file.close()
